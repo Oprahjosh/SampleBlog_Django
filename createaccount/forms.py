a5cb2f3eb10dc85d4f76466cc7dbceee2a554415
DevOpes <65594389+Oprahjosh@users.noreply.github.com>
2021-05-06 22:52:09 +0100

    Add files via upload

---
+++ createaccount/forms.py
@@ -0,0 +1,19 @@
+from django import forms
+#from django.contrib.auth import registration,authenticate
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from myBlog.models import  Comment
+
+
+class RegisterForm(UserCreationForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username','email','password1','password2']
+
+class CommentForm(forms.ModelForm):
+
+    class Meta:
+        model = Comment
+        fields = ['name', 'email', 'body',]
